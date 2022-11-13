fbd16a7d10d6a6da71bdfc7940d030426f6a21d3
Daniel Holmes <daniel.holmes@softwire.com>
2022-11-13 17:03:07 +0000

    Bug fix

--- scripts/guestList.js
+++ scripts/guestList.js
@@ -1,12 +1,12 @@
 const getGuestList = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const password = urlParams.get('password');
-    fetch(`https://w6f2wu6d64li4mgei2wrg7z5pa0zueeh.lambda-url.eu-west-2.on.aws/?TableName=wedding_guests${password ? `&password=${password}` : ''}`)
+    return fetch(`https://w6f2wu6d64li4mgei2wrg7z5pa0zueeh.lambda-url.eu-west-2.on.aws/?TableName=wedding_guests${password ? `&password=${password}` : ''}`)
         .then(response => response.json())
         .then(data => {
             guests = data.Items;
             guests.sort((a, b) => (a.name > b.name) ? 1 : -1);
             guests.reverse();
             return guests;
-        })
+        });
     }
